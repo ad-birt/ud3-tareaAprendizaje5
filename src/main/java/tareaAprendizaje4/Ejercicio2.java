68c2ba8d74b9822e442aad86b34759559b4ffa88
bmadina <bmadina@birt.eus>
2021-10-01 14:48:02 +0200

    initial commit

---
+++ src/main/java/tareaAprendizaje4/Ejercicio2.java
@@ -0,0 +1,42 @@
+package tareaAprendizaje4;
+
+import com.zaxxer.hikari.HikariDataSource;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.time.LocalDateTime;
+
+public class Ejercicio2 {
+
+    public static void main(String[] args) throws SQLException {
+        DataSource ds = createDataSource();
+
+        try (Connection connection = ds.getConnection()) {
+
+            try (PreparedStatement stmt = connection.prepareStatement("select * " +
+                    "from users")) {
+
+                ResultSet resultSet = stmt.executeQuery();
+                while (resultSet.next()) {
+                    int id = resultSet.getInt("id");
+                    String firstName = resultSet.getString("first_name");
+                    String lastName = resultSet.getString("last_name");
+                    LocalDateTime registrationDate = resultSet.getObject(
+                            "registration_date", LocalDateTime.class);
+
+                    System.out.println("Found user: " + id + " | " + firstName +
+                            " | " + lastName + " | " + registrationDate);
+                }
+            }
+        }
+    }
+
+    private static DataSource createDataSource() {
+        HikariDataSource ds = new HikariDataSource();
+        ds.setJdbcUrl("jdbc:h2:~/mydatabase;INIT=RUNSCRIPT FROM 'classpath:schema" +
+                ".sql'");
+        ds.setUsername("sa");
+        ds.setPassword("s3cr3tPassword");
+        return ds;
+    }
+}
